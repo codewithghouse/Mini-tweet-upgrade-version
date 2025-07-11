cef55e0a7304762b7814eefd52259698dd8b2e3c
codewithghouse <personalghouse@gmail.com>
2025-07-11 12:17:56 +0530

    h

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express= require("express");
 const app= express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const path = require("path");
 const { faker } = require('@faker-js/faker');
 const { v4: uuidv4 } = require('uuid');
