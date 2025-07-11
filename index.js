eeecf24a104bb980c3db3c001ee7cf110261016a
codewithghouse <personalghouse@gmail.com>
2025-07-11 11:52:24 +0530

    Fix start script and optimize package.json for deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express= require("express");
 const app= express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const path = require("path");
 const { faker } = require('@faker-js/faker');
 const { v4: uuidv4 } = require('uuid');
